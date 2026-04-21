98c72f3678955ee4296c9d4c87cbf00c59034211
SEKI-YUTA <zard.since1991@gmail.com>
2026-04-21 22:33:33 +0900

    fix(ui): apply theme tint to view mode toggle menu icon in dark mode

--- app/src/main/java/code/name/monkey/retromusic/fragments/base/AbsRecyclerViewCustomGridSizeSwitchableViewModeFragment.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/base/AbsRecyclerViewCustomGridSizeSwitchableViewModeFragment.kt
@@ -14,11 +14,6 @@
  */
 package code.name.monkey.retromusic.fragments.base
 
-import code.name.monkey.retromusic.compose.AlbumJacket
-import code.name.monkey.retromusic.compose.Ios6LikeLazyRow
-import code.name.monkey.retromusic.compose.SongCard
-import code.name.monkey.retromusic.compose.SongsList
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
@@ -27,77 +22,38 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.animation.Crossfade
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PageSize
-import androidx.compose.foundation.pager.PagerDefaults
-import androidx.compose.foundation.pager.PagerSnapDistance
-import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.util.lerp
-import androidx.core.net.toUri
 import androidx.core.os.bundleOf
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import code.name.monkey.appthemehelper.common.ATHToolbarActivity
+import code.name.monkey.appthemehelper.util.ToolbarContentTintHelper
 import code.name.monkey.retromusic.EXTRA_ALBUM_ID
 import code.name.monkey.retromusic.R
-import code.name.monkey.retromusic.helper.MusicPlayerRemote
+import code.name.monkey.retromusic.compose.Ios6LikeLazyRow
+import code.name.monkey.retromusic.compose.SongsList
+import code.name.monkey.retromusic.extensions.getTintedDrawable
 import code.name.monkey.retromusic.model.Album
 import code.name.monkey.retromusic.model.AlbumHorizontalPagerModel
-import code.name.monkey.retromusic.model.Song
 import code.name.monkey.retromusic.util.MusicUtil
 import code.name.monkey.retromusic.util.PreferenceUtil
 import code.name.monkey.retromusic.util.theme.RetroTheme
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
-import com.bumptech.glide.integration.compose.placeholder
-import kotlin.math.absoluteValue
 
 abstract class AbsRecyclerViewCustomGridSizeSwitchableViewModeFragment<A : RecyclerView.Adapter<*>, LM : RecyclerView.LayoutManager> :
     AbsRecyclerViewCustomGridSizeFragment<A, LM>() {
@@ -113,10 +69,6 @@ abstract class AbsRecyclerViewCustomGridSizeSwitchableViewModeFragment<A : Recyc
         return super.onCreateView(inflater, container, savedInstanceState)
     }
 
-    private val demoSongData = List(10) {
-        "Song No.$it"
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         updateViewVisibility()
@@ -130,65 +82,65 @@ abstract class AbsRecyclerViewCustomGridSizeSwitchableViewModeFragment<A : Recyc
             setContent {
                 RetroTheme {
                     Scaffold {
-                    val jacketUriList =
-                        produceState(initialValue = emptyList()) {
-                            libraryViewModel.getAlbums().observe(viewLifecycleOwner) { albums ->
-                                value = albums.map { album ->
-                                    AlbumHorizontalPagerModel(
-                                        albumId = album.id,
-                                        albumName = album.title,
-                                        albumArtist = album.albumArtist,
-                                        jacketImageUri = MusicUtil.getMediaStoreAlbumCoverUri(
-                                            album.id
+                        val jacketUriList =
+                            produceState(initialValue = emptyList()) {
+                                libraryViewModel.getAlbums().observe(viewLifecycleOwner) { albums ->
+                                    value = albums.map { album ->
+                                        AlbumHorizontalPagerModel(
+                                            albumId = album.id,
+                                            albumName = album.title,
+                                            albumArtist = album.albumArtist,
+                                            jacketImageUri = MusicUtil.getMediaStoreAlbumCoverUri(
+                                                album.id
+                                            )
                                         )
-                                    )
+                                    }
                                 }
                             }
-                        }
-                    Column(
-                        modifier = Modifier.padding(
-                            bottom = (it.calculateTopPadding().value + it.calculateBottomPadding().value + dimensionResource(
-                                R.dimen.bottom_nav_mini_player_height
-                            ).value).dp
-                        )
-                    ) {
-                        val pagerState =
-                            rememberPagerState(pageCount = { jacketUriList.value.size })
-                        var currentShowingAlbum by remember {
-                            mutableStateOf<Album?>(null)
-                        }
-                        LaunchedEffect(pagerState.settledPage, jacketUriList.value.size) {
-                            if (jacketUriList.value.isEmpty()) return@LaunchedEffect
-                            currentShowingAlbum = libraryViewModel.albumById(
-                                jacketUriList.value[pagerState.currentPage].albumId
+                        Column(
+                            modifier = Modifier.padding(
+                                bottom = (it.calculateTopPadding().value + it.calculateBottomPadding().value + dimensionResource(
+                                    R.dimen.bottom_nav_mini_player_height
+                                ).value).dp
                             )
-                        }
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Ios6LikeLazyRow(
-                            pagerState = pagerState,
-                            dataSet = jacketUriList.value,
-                            onClickAlbumCard = { albumId ->
-                                if (oldAlbumView != null) FragmentNavigatorExtras(
-                                    oldAlbumView!! to albumId.toString()
-                                ) else null
-                                findNavController().navigate(
-                                    R.id.albumDetailsFragment,
-                                    bundleOf(EXTRA_ALBUM_ID to albumId),
-                                    null,
-                                )
-
+                        ) {
+                            val pagerState =
+                                rememberPagerState(pageCount = { jacketUriList.value.size })
+                            var currentShowingAlbum by remember {
+                                mutableStateOf<Album?>(null)
                             }
+                            LaunchedEffect(pagerState.settledPage, jacketUriList.value.size) {
+                                if (jacketUriList.value.isEmpty()) return@LaunchedEffect
+                                currentShowingAlbum = libraryViewModel.albumById(
+                                    jacketUriList.value[pagerState.currentPage].albumId
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(16.dp))
+                            Ios6LikeLazyRow(
+                                pagerState = pagerState,
+                                dataSet = jacketUriList.value,
+                                onClickAlbumCard = { albumId ->
+                                    if (oldAlbumView != null) FragmentNavigatorExtras(
+                                        oldAlbumView!! to albumId.toString()
+                                    ) else null
+                                    findNavController().navigate(
+                                        R.id.albumDetailsFragment,
+                                        bundleOf(EXTRA_ALBUM_ID to albumId),
+                                        null,
+                                    )
 
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Crossfade(
-                            targetState = currentShowingAlbum,
-                            modifier = Modifier.weight(1f)
-                        ) { album ->
-                            SongsList(album)
+                                }
+
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            Crossfade(
+                                targetState = currentShowingAlbum,
+                                modifier = Modifier.weight(1f)
+                            ) { album ->
+                                SongsList(album)
+                            }
                         }
                     }
-                }
                 }
             }
         }
@@ -213,9 +165,21 @@ abstract class AbsRecyclerViewCustomGridSizeSwitchableViewModeFragment<A : Recyc
             .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
     }
 
+    private fun updateToggleViewModeIcon(item: MenuItem, isOldAppleAlbumView: Boolean) {
+        val icon = if (isOldAppleAlbumView) {
+            R.drawable.ic_snap_list
+        } else {
+            R.drawable.ic_grid_view
+        }
+        val toolbarColor = ATHToolbarActivity.getToolbarBackgroundColor(toolbar)
+        val tintColor = ToolbarContentTintHelper.toolbarContentColor(requireContext(), toolbarColor)
+        item.icon = requireContext().getTintedDrawable(icon, tintColor)
+    }
+
     override fun onMenuItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.action_toggle_album_view_mode) {
             PreferenceUtil.isOldAppleAlbumViewEnabled = !PreferenceUtil.isOldAppleAlbumViewEnabled
+            updateToggleViewModeIcon(item, PreferenceUtil.isOldAppleAlbumViewEnabled)
             updateViewVisibility()
             return true
         }
